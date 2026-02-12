322be7527c2c21bbf9aee2e83df7f83c903258f1
Mohit Chauhan <130385014+chauhanmohit12@users.noreply.github.com>
2026-02-12 18:43:22 +0530

    Refactor comments and maintain script functionality
    Updated comments for clarity and maintained functionality.

--- script.py
+++ script.py
@@ -3,12 +3,12 @@ from PIL import ImageGrab
 
 pyautogui.FAILSAFE = False
 
-def take_screenshot():
+def take_screenshot():#only for debugging
     img = ImageGrab.grab()
     img.save(f"{str(datetime.datetime.now()).replace(':','-')[0:16]}.png")
 
 
-def move(key1,key2,key3):
+def move(key1,key2,key3):#moves character along x, y and z axis
     keyboard.press(key1)
     keyboard.press(key2)
     keyboard.press(key3)
@@ -23,30 +23,29 @@ print("starting")
 
 while True:
     
-    keyboard.press_and_release("ctrl+windows+right")
+    keyboard.press_and_release("ctrl+windows+right")#change desktop
     time.sleep(0.5)
     keyboard.press_and_release("space")
     for i in range(3):
-        pyautogui.click(960,540)
-    # pyautogui.click(960,540)
+        pyautogui.click(960,540)#for focus change and attacking
 
     time.sleep(0.5)
     
+    #key bind declaration
     arr1 = ["w","s"]
     arr2 = ["a","d"]
     arr3 = ["space"]
     
+    #ramdom movement values 
     k1 = random.choice(arr1)
     k2 = random.choice(arr2)
     k3 = random.choice(arr3)
-    
     move(k1,k2,k3)
-    print(k1,k2,k3)
     
     time.sleep(1)
     print(str(datetime.datetime.now()).replace(":","-")[0:16])
     # take_screenshot()
     
-    keyboard.press_and_release("ctrl+windows+left")
+    keyboard.press_and_release("ctrl+windows+left")#change desktop
     
-    time.sleep(300)
+    time.sleep(300)#waits for 5 mins 
